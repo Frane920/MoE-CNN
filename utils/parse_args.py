1fec5504bbdb2ffac7fc442fd9fbcd087b293204
Frane920 <frrraneck2@gmail.com>
2025-09-28 18:35:00 +0200

    Changed default args

--- utils/parse_args.py
+++ utils/parse_args.py
@@ -1,15 +1,14 @@
 import argparse
 
-
 def parse_args():
     """Parse command line arguments."""
     p = argparse.ArgumentParser()
     p.add_argument('--data-dir', type=str, default='./data')
-    p.add_argument('--per_gpu_batch', type=int, default=256)
-    p.add_argument('--epochs', type=int, default=120)
+    p.add_argument('--per_gpu_batch', type=int, default=512)
+    p.add_argument('--epochs', type=int, default=125)
     p.add_argument('--lr', type=float, default=3e-5)
-    p.add_argument('--accum', type=int, default=2)
-    p.add_argument('--num_workers', type=int, default=12)
+    p.add_argument('--accum', type=int, default=4)
+    p.add_argument('--num_workers', type=int, default=8)
     p.add_argument('--save', type=str, default='model.pt')
     p.add_argument('--max_train', type=int, default=None)
     p.add_argument('--max_test', type=int, default=None)
@@ -27,7 +26,7 @@ def parse_args():
     p.add_argument('--use_focal', action='store_true', default=True)
     p.add_argument('--cutmix_alpha', type=float, default=1)
     p.add_argument('--mixup_alpha', type=float, default=0.8)
-    p.add_argument('--resume', type=str, default='bestmodel.pt/best_model.pt')
+    p.add_argument('--resume', type=str, default='best_model.pt')
     p.add_argument('--weight_decay', type=float, default=5e-4)
     p.add_argument('--distributed', action='store_true')
     p.add_argument('--resize', type=int, default=None)
@@ -40,8 +39,8 @@ def parse_args():
     p.add_argument('--penalty_weight', type=float, default=0.2)
     p.add_argument('--gradient_checkpointing', action='store_true', default=False)
     p.add_argument('--no_prefetch', action='store_true', default=False)
-    p.add_argument('--swa_start', type=int, default=None, help='Epoch to start SWA (defaults to 75% of total epochs)')
-    p.add_argument('--k_per_specialization', type=int, default=2, help='Number of experts to select per specialization')
+    p.add_argument('--swa_start', type=int, default=100, help='Epoch to start SWA (defaults to 75% of total epochs)')
+    p.add_argument('--k_per_specialization', type=int, default=1, help='Number of experts to select per specialization')
     p.add_argument('--use_specialized_moe', action='store_true', default=True,
                    help='Use specialized MoE with unknown class filtering')
     p.add_argument('--unknown_threshold', type=float, default=0.3,
