88cd70f8d62cf14c5c656352e4a530e114dcaf3b
Frane920 <128966952+Frane920@users.noreply.github.com>
2025-09-29 21:28:55 +0200

    Update MoE.py
    Extend gate_shared architecture

--- models/MoE.py
+++ models/MoE.py
@@ -38,20 +38,25 @@ class MoE(nn.Module):
         ])
 
         self.gate_shared = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Linear(64, 128),
-            nn.BatchNorm1d(128),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Dropout(0.3)
-        )
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU()
+    )
 
         self.gate_digit = nn.Linear(128, num_digit_experts)
         self.gate_upper = nn.Linear(128, num_uppercase_experts)
